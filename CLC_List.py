b6c7f97974c94df00643fbb96acaba3b3905c3cf
epicary22 <89352235+epicary22@users.noreply.github.com>
2022-04-01 19:32:00 -0500

    Create CLC_List.py

---
+++ CLC_List.py
@@ -0,0 +1,447 @@
+# German Dictionary (Conjuguemos-like), dict (dictionary) CX (Chapter X) S1 (Section 1)
+
+dictC1S1 = {
+    1: ["die Stereotypen", "the stereotypes"],
+    2: ["die Vorurteile", "the prejudices"],
+    3: ["der Amerikaner", "the American"],
+    4: ["der Teenager", "the teenager"],
+    5: ["die Frau", "the woman"],
+    6: ["der Mann", "the man"],
+    7: ["der Katholik", "the Catholic"],
+    8: ["der Protestant", "the Protestant"],
+    9: ["der Christ", "the Christian"],
+    10: ["der Jude", "the Jew"],
+    11: ["andere Religion", "another religion"],
+    12: ["weiß", "white"],
+    13: ["der Mexikanisch-Amerikaner", "the Mexican-American"],
+    14: ["der Afro-Amerikaner", "the African-American"],
+    15: ["hispanisch", "Hispanic"],
+    16: ["der Indianer", "the Native American"],
+    17: ["andere Rasse", "another race"],
+    18: ["der Schüler", "the student"],
+    19: ["der Athlet", "the athlete"],
+    20: ["der Musiker", "the musician"],
+    21: ["der Sänger", "the singer"],
+    22: ["das Computergenie", "the computer genius"],
+    23: ["der Cheerleader", "the cheerleader"],
+    24: ["mein Name", "my name"],
+    25: ["der Mensch", "the human"],
+    26: ["etwas Anderes", "something else"],
+    27: ["das Individuum", "the induvidual"]
+    }
+
+dictC1S2 = {
+    1: ["die Iren", "the Irish"],
+    2: ["die Italiener", "the Italians"],
+    3: ["die Polen", "the Poles"],
+    4: ["die Deutsche", "the Germans"],
+    5: ["die Latinos", "the Latinos"],
+    6: ["die Afroamerikaner", "the African-Americans"],
+    7: ["die Weiße", "the Whites"],
+    8: ["die Indianer", "the Native Americans"],
+    9: ["die Südstaatler", "the southerners"],
+    10: ["die Nordstaatler", "the northerners"],
+    11: ["die Linkshänder", "the lefties"],
+    12: ["die Rechtshänder", "the righties"],
+    13: ["arme Leute", "poor people"],
+    14: ["reiche Leute", "rich people"],
+    15: ["die Frauen", "the women"],
+    16: ["die Männer", "the men"],
+    17: ["die Schüler", "the students"],
+    18: ["die Lehrer", "the teachers"],
+    19: ["die Politiker", "the politicians"],
+    20: ["die Athleten", "the athletes"],
+    21: ["die Polizisten", "the policemen"],
+    22: ["dicke Leute", "fat people"],
+    23: ["dünne Leute", "thin people"],
+    24: ["die Teenager", "the teenagers"]
+    }
+
+dictC1S3 = {
+    1: ["weinen", "to cry"],
+    2: ["heimlich", "secretly"],
+    3: ["die Zärtlichkeit", "the tenderness"],
+    4: ["verletzlich", "vulnerable"],
+    5: ["unersetzlich", "irreplaceable"],
+    6: ["der Strom", "the electric current"],
+    7: ["das Geld", "the money"],
+    8: ["schwer", "difficult"],
+    9: ["die Geborgenheit", "the safety"]
+    }
+
+dictC1S4 = {
+    1: ["nehmen", "to take"],
+    2: ["geben", "to give"],
+    3: ["brauchen", "to need"],
+    4: ["viel", "many"],
+    5: ["kaufen", "to buy"],
+    6: ["stehen", "to stand"],
+    7: ["durch", "through"],
+    8: ["werden", "to become"],
+    9: ["wann", "when"],
+    10: ["können", "can"],
+    11: ["müssen", "must"],
+    12: ["kein", "none"]
+    }
+
+dictC1S5 = {
+    1: ["die Eifersucht", "the jealousy"],
+    2: ["der Neid", "the envy"],
+    3: ["betrunken", "drunk"]
+    }
+
+dictC1 = {
+    1: dictC1S1,
+    2: dictC1S2,
+    3: dictC1S3,
+    4: dictC1S4,
+    5: dictC1S5
+    }
+
+dictC2S1 = {
+    1: ["wichtig", "important"],
+    2: ["die Stimme", "the voice"],
+    3: ["stören", "to disturb"],
+    4: ["die Zeitung", "the newspaper"],
+    5: ["erinnern", "to remember"],
+    6: ["aufgeregt", "excited"],
+    7: ["erklären", "to explain"],
+    8: ["Ich habe keine Ahnung.", "I have no idea."],
+    9: ["umgebracht", "murdered"],
+    10: ["Was ist los?", "What's happening?"]
+    }
+
+dictC2S2 = {
+    1: ["die Fahrt", "the trip"],
+    2: ["zum letzten Mal", "for the last time"],
+    3: ["der Beruf", "the profession"],
+    4: ["verdienen", "to earn"],
+    5: ["genau", "exactly"],
+    6: ["halten", "to hold"],
+    7: ["hielt", "held"],
+    8: ["aussteigen", "to get out"],
+    9: ["stieg aus", "got out"],
+    10: ["neugierig", "curious"],
+    11: ["der Eingang", "the entrance"],
+    12: ["Tut mir leid.", "I'm sorry."],
+    13: ["verboten", "prohibited"],
+    14: ["dauern", "to last"],
+    15: ["schließlich", "eventually"],
+    16: ["auf keinen Fall", "in no case"],
+    17: ["nicken", "to nod"],
+    18: ["ohne", "without"]
+    }
+
+dictC2S3 = {
+    1: ["passieren", "to happen"],
+    2: ["erzählen", "to tell"],
+    3: ["verschlossen", "locked"],
+    4: ["der Schlüssel", "the key"],
+    5: ["holen", "to get"],
+    6: ["außer", "except"],
+    7: ["liegen", "to lay"],
+    8: ["lag", "laid"],
+    9: ["tot", "dead"],
+    10: ["erschossen", "shot"],
+    11: ["der Selbstmord", "the suicide"],
+    12: ["gewesen", "been"],
+    13: ["ein an mich gerichtetes Entlassungschreiben", "a letter of dismissal addressed to me"],
+    14: ["umgebracht", "murdered"],
+    15: ["verlassen", "lonely"]
+    }
+
+dictC2S4 = {
+    1: ["verheiratet mit", "married to"],
+    2: ["sterben", "to die"],
+    3: ["starb", "died"],
+    4: ["der Brief", "the letter"]
+    }
+
+dictC2S5 = {
+    1: ["beschließen", "to resolve"],
+    2: ["beschloß", "resolved"],
+    3: ["besuchen", "to visit"],
+    4: ["wechseln", "to change"],
+    5: ["die Schwierigkeit", "the trouble"],
+    6: ["heißen", "to be called"],
+    7: ["hieß", "called"],
+    8: ["der Teppich", "the carpet"],
+    9: ["der Teil", "the part"],
+    10: ["sauber", "clean"],
+    11: ["putzen", "to clean"]
+    }
+
+dictC2S6 = {
+    1: ["finden", "to find"],
+    2: ["fand", "found"],
+    3: ["geben", "to give"],
+    4: ["gab", "gave"],
+    5: ["kommen", "to come"],
+    6: ["kam", "came"],
+    7: ["lesen", "to read"],
+    8: ["las", "read"],
+    9: ["stehen", "to stand"],
+    10: ["stand", "stood"],
+    11: ["bleiben", "to stay"],
+    12: ["blieb", "stayed"],
+    13: ["gehen", "to go"],
+    14: ["ging", "went"],
+    15: ["schreiben", "to write"],
+    16: ["schrieb", "wrote"],
+    17: ["werden", "to become"],
+    18: ["wurde", "became"],
+    19: ["bringen", "to bring"],
+    20: ["brachte", "brought"],
+    21: ["denken", "to think"],
+    22: ["dachte", "thought"],
+    23: ["kennen", "to know"],
+    24: ["kannte", "knew"],
+    25: ["wissen", "to know"],
+    26: ["wusste", "knew"],
+    27: ["seinen Kopf durchsetzen", "to have his will"],
+    28: ["die Nase voll haben", "to have enough"],
+    29: ["einsperren", "to lock up"],
+    30: ["zufrieden", "content"],
+    31: ["gestorben", "died"]
+    }
+
+dictC2S7 = {
+    1: ["die Zusammenfassung", "the summary"],
+    }
+
+dictC2S8 = {
+    1: ["verheiratet", "married"],
+    2: ["das Gerücht", "the rumor"],
+    3: ["der Seitensprung", "the affair"],
+    4: ["führen", "to lead"],
+    5: ["versuchen", "to try"],
+    6: ["die Kneipe", "the pub"],
+    7: ["sich verabreden", "to make an appointment"],
+    8: ["verstehen", "to understand"],
+    9: ["verstand", "understood"],
+    10: ["überprüfen", "to examine"],
+    11: ["sich kümmern um", "to take care of"],
+    12: ["der Vorschuss", "the advance payment"],
+    13: ["das Glück", "the luck"]
+    }
+
+dictC2S9 = {
+    1: ["eintreffen", "to arrive"],
+    2: ["eintraf", "arrived"],
+    3: ["die Spur", "the lead"],
+    4: ["noch einmal", "one more time"],
+    5: ["anschließend", "affiliated"],
+    6: ["mitteilen", "to inform"],
+    7: ["es steht eindeutig fest", "it is a proven fact"],
+    8: ["leider", "unfortunately"],
+    9: ["weder... noch...", "neither... nor..."],
+    10: ["verändern", "to alter"],
+    11: ["niedergeschlagen", "depressed"]
+    }
+
+dictC2S10 = {
+    1: ["der Arzt", "the doctor"],
+    }
+
+dictC2S11 = {
+    1: ["vornehm", "elegant"],
+    2: ["die Hoffnung", "the hope"],
+    3: ["hässlich", "ugly"],
+    4: ["müde", "tired"],
+    5: ["Es tut mir Leid.", "I'm sorry."],
+    6: ["sprechen", "to speak"],
+    7: ["sprach", "spoke"],
+    8: ["gesprochen", "have spoken"],
+    9: ["wieso", "how come"],
+    10: ["der Feind", "the enemy"],
+    11: ["vorstellen", "to imagine"],
+    12: ["das Gefühl", "the feeling"],
+    13: ["wirken", "to seem"],
+    14: ["einwandfrei", "beyond reproach"],
+    15: ["das Steuer", "the steering wheel"]
+    }
+
+dictC2S12 = {
+    1: ["beschließen", "to resolve"],
+    2: ["beschloss", "resolved"],
+    3: ["beschlossen", "have resolved"],
+    4: ["der Bekannte", "the acquaintance"],
+    5: ["das Glück", "the luck"],
+    6: ["zeigen", "to display"],
+    7: ["das stimmt", "that's true"],
+    8: ["das Finanzamt", "the German IRS"],
+    9: ["die Wahrheit", "the truth"],
+    10: ["langsam", "slow"],
+    11: ["fast", "almost"],
+    12: ["wählen", "to choose"],
+    13: ["niemand", "no one"],
+    14: ["auflegen", "to hang up the phone"],
+    15: ["ohne", "without"],
+    16: ["der Besitzer", "the owner"]
+    }
+
+dictC2S13 = {
+    1: ["schätzen", "to estimate"],
+    2: ["ruhig", "calm"],
+    3: ["verlassen", "lonely"],
+    4: ["wiederholen", "to repeat"],
+    5: ["ungefähr", "approximately"],
+    6: ["zum letzten Mal", "for the last time"],
+    7: ["benutzen", "to use"],
+    8: ["aussehen", "to look like"],
+    9: ["und ob", "you bet!"],
+    10: ["der Befehl", "the order"],
+    11: ["die Verzweiflung", "the despair"],
+    12: ["die Neugier", "the curiosity"]
+    }
+
+dictC2S14 = {
+    1: ["schlecht gelaunt", "in a bad mood"],
+    2: ["annehmen", "to assume"],
+    3: ["klappen", "to succeed"],
+    4: ["vertuschen", "to conceal"],
+    5: ["der Schlüssel", "the key"],
+    6: ["das Schmuckstück", "the piece of jewelry"],
+    7: ["die Eifersucht", "the envy"],
+    8: ["die Geliebte", "the lover"],
+    9: ["sich rächen", "to take revenge"],
+    10: ["der Verdächtige", "the suspect"],
+    11: ["die Spur", "the lead"],
+    12: ["sofort", "immediately"],
+    13: ["der Zweck", "the purpose"]
+    }
+
+dictC2 = {
+    1: dictC2S1,
+    2: dictC2S2,
+    3: dictC2S3,
+    4: dictC2S4,
+    5: dictC2S5,
+    6: dictC2S6,
+    7: dictC2S8,
+    8: dictC2S9,
+    9: dictC2S11,
+    10: dictC2S12,
+    11: dictC2S13,
+    12: dictC2S14
+    }
+
+dictC3S2 = {
+    1: ["das Ende kommt heran", "the end is drawing near"],
+    2: ["das Töchterlein", "the little daughter"],
+    3: ["fromm", "pious"],
+    4: ["beistehen", "to stand by"],
+    5: ["herabblicken", "to look down"],
+    6: ["verscheiden", "to pass away"],
+    7: ["das Grab", "the grave"],
+    8: ["das Angesicht", "the face"],
+    9: ["garstig", "foul"],
+    10: ["das Stiefkind", "the stepchild"]
+    }
+
+dictC3S4 = {
+    1: ["die Flasche", "the bottle"],
+    2: ["treffen", "to meet"],
+    3: ["traf", "met"],
+    4: ["erzählen", "to tell"],
+    5: ["bunt", "colorful"],
+    6: ["der Strauß", "the bouquet"],
+    7: ["sich freuen", "to be glad"],
+    8: ["pflücken", "to pluck"],
+    9: ["verschlingen", "to devour"],
+    10: ["verschlang", "devoured"],
+    11: ["erstaunt", "amazed"],
+    12: ["fremd", "foreign"],
+    13: ["aussehen", "to look like"],
+    14: ["sah aus", "looked like"],
+    15: ["packen", "to seize"],
+    16: ["entsetzlich", "terrible"],
+    17: ["das Maul", "the maw"],
+    18: ["schnarchen", "to snore"],
+    19: ["der Bauch", "the belly"],
+    20: ["nähen", "to sew"],
+    21: ["aufwachen", "to wake up"]
+    }
+
+dictC3 = {
+    1: dictC3S2,
+    2: dictC3S4
+    }
+
+dictC4S1 = {
+    1: ["die Identität", "the identity"],
+    2: ["können", "can"],
+    3: ["tatsächlich", "actual"],
+    4: ["beantworten", "to reply"],
+    5: ["aussuchen", "to choose"],
+    6: ["sich verändern", "to alter"],
+    7: ["die Klassengegensätze", "the class differences"],
+    8: ["das Jahrhundert", "the century"],
+    9: ["sich verwischen", "to obliterate"],
+    10: ["festlegen", "to determine"],
+    11: ["die Mode", "the fashion"],
+    12: ["die Selbstdarstellung", "the self-expression"],
+    13: ["das Umfallen", "the drop"],
+    14: ["weder... noch...", "neither... nor..."],
+    15: ["verleihen", "to award"],
+    16: ["der Wert", "the value"]
+    }
+
+dictC4S2 = {
+    1: ["das Mittelalter", "the Middle Ages"],
+    2: ["das Jahrhundert", "the century"],
+    3: ["sich kümmern um...", "to care for..."],
+    4: ["der Mönch", "the monk"],
+    5: ["die Nonne", "the nun"],
+    6: ["bebauen", "to farm"],
+    7: ["das Nonnenkloster", "the convent"],
+    8: ["verdenken", "to owe"],
+    9: ["unterstützen", "to support"],
+    10: ["die Angelegenheit", "the matter"],
+    11: ["die Gewalt", "the force"],
+    12: ["weltlich", "worldly"],
+    13: ["der Reichtum", "the riches"],
+    14: ["das Seelenheil", "the salvation"],
+    15: ["reformieren", "to reform"],
+    16: ["die Freude", "the joy"],
+    17: ["der Landsknecht", "the mercenary"],
+    18: ["der Ritter", "the knight"],
+    19: ["nicht nur... sondern auch...", "not only... but also..."],
+    20: ["die Wanderschaft", "the wanderings"]
+    }
+
+
+dictC4 = {
+    1: dictC4S1,
+    2: dictC4S2
+    }
+
+dictAll = {
+    1: dictC1,
+    2: dictC2,
+    3: dictC3,
+    4: dictC4
+    }
+
+dictCopyable = {
+    1: ["", ""],
+    2: ["", ""],
+    3: ["", ""],
+    4: ["", ""],
+    5: ["", ""],
+    6: ["", ""],
+    7: ["", ""],
+    8: ["", ""],
+    9: ["", ""],
+    10: ["", ""],
+    11: ["", ""],
+    12: ["", ""],
+    13: ["", ""],
+    14: ["", ""],
+    15: ["", ""],
+    16: ["", ""],
+    17: ["", ""],
+    18: ["", ""],
+    19: ["", ""],
+    20: ["", ""]
+    }
